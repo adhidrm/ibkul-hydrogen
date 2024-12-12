c43feca9e0470bf240509ebe97f64f5f0e30278e
Adhi <darkseekertm@gmail.com>
2024-12-12 08:16:20 +0700

    add unsafe-inline into script src elm aray

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -23,10 +23,12 @@ export default async function handleRequest(
       storeDomain: context.env.PUBLIC_STORE_DOMAIN,
     },
     imgSrc: ['https://cdn.builder.io', 'https://cdn.shopify.com'],
-    scriptSrcElem: ['https://cdn.builder.io', 'https://cdn.shopify.com', `nonce-${nonce}`],
+    scriptSrcElem: ['https://cdn.builder.io', 'https://cdn.shopify.com', 'unsafe-inline'],
     fontSrc: ['https://fonts.gstatic.com/'], 
   });
-
+  // Add the nonce to the scriptSrcElem array
+  // scriptSrcElem.push(`nonce-${nonce}`);
+  
   const body = await renderToReadableStream(
     <NonceProvider>
       <RemixServer context={remixContext} url={request.url} />
